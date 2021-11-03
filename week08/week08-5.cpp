828a35f716b7f0989316fd7e3220629e690ecbec
mike7862 <91170810+mike7862@users.noreply.github.com>
2021-11-03 12:01:13 +0800

    Add files via upload

---
+++ week08/week08-5.cpp
@@ -0,0 +1,19 @@
+///week08-5.cpp step03-2
+#include <stdio.h>
+int main()
+{   ///三角形
+    printf("1: *\n");///1樓,1顆星
+    printf("1: **\n");///2樓,2顆星
+    printf("1: ***\n");///3樓,3顆星
+    printf("1: ****\n");///4樓,4顆星
+    printf("1: *****\n");///5樓,5顆星
+    printf("1: ******\n");///6樓,6顆星
+    ///高度越高,程式越多... ex. 40層
+    int n=6;
+    for(int i=1; i<=n; i++){///先建出樓層
+        printf("%d: ", i);///第i樓
+        for(int k=1; k<=i; k++) printf("*");///第i樓,i顆星
+        printf("\n");
+    }
+}
+
