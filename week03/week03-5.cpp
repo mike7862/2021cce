147d667fcc22a4fabfbe424cf4af4566046c063c
mike7862 <91170810+mike7862@users.noreply.github.com>
2021-12-01 19:45:34 +0800

    Delete week03 directory

--- week03/week03-5.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    printf("hello world\n");
-    printf("hello%dworld\n",300);
-    printf("%d %d    %d ",300,200,100);
-}
--- week03/week03-6.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int a;
-    printf("請輸入一個值a:");
-    scanf("%d", &a);
-    printf("a的值是:%d\n",a);
-
-}
