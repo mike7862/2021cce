9d0e5ba1cc5dc233749e2a54f439f997c1d4182b
mike7862 <91170810+mike7862@users.noreply.github.com>
2021-12-01 19:39:26 +0800

    Add files via upload

---
+++ week03/week03-5.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    printf("hello world\n");
+    printf("hello%dworld\n",300);
+    printf("%d %d    %d ",300,200,100);
+}
---
+++ week03/week03-6.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("請輸入一個值a:");
+    scanf("%d", &a);
+    printf("a的值是:%d\n",a);
+
+}
