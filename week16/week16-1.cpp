bf0f691e8977951863cf2e1649260da15cd90429
mike7862 <91170810+mike7862@users.noreply.github.com>
2021-12-29 11:55:59 +0800

    Add files via upload

---
+++ week16/week16-1.cpp
@@ -0,0 +1,13 @@
+///week16-1.cpp step01-1 字串
+#include <stdio.h>
+int a[100];///整數陣列
+char s[20];///字母的陣列,字串
+
+int main()
+{
+    printf("請輸入你的名子: ");
+    scanf("%s", s );
+    ///Q: scanf("%d", &n );
+
+    printf("你剛剛輸入:%s\n", s );
+}
